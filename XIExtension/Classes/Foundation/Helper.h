df12c5d0d4ccb935834770c4f08dcb52f459f64e
sooxie <xiezhiping13179@gmail.com>
2021-03-23 17:07:43 +0800

    add

---
+++ XIExtension/Classes/Foundation/Helper.h
@@ -0,0 +1,18 @@
+//
+//  Helper.h
+//  AwesomeIOS
+//
+//  Created by fangxiang on 2020/2/9.
+//  Copyright © 2020 sooxie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Helper : NSObject
++(NSString *)jsonStringWithDict:(NSDictionary *)dict;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XIExtension/Classes/Foundation/Helper.m
@@ -0,0 +1,51 @@
+//
+//  Helper.m
+//  AwesomeIOS
+//
+//  Created by fangxiang on 2020/2/9.
+//  Copyright © 2020 sooxie. All rights reserved.
+//
+
+#import "Helper.h"
+
+@implementation Helper
+// 字典转json字符串方法
++(NSString *)jsonStringWithDict:(NSDictionary *)dict
+
+{
+    
+    NSError *error;
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:&error];
+    
+    NSString *jsonString;
+    
+    if (!jsonData) {
+        
+        NSLog(@"%@",error);
+        
+    }else{
+        
+        jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+        
+    }
+    
+    NSMutableString *mutStr = [NSMutableString stringWithString:jsonString];
+    
+    NSRange range = {0,jsonString.length};
+    
+    //去掉字符串中的空格
+    
+    [mutStr replaceOccurrencesOfString:@" " withString:@"" options:NSLiteralSearch range:range];
+    
+    NSRange range2 = {0,mutStr.length};
+    
+    //去掉字符串中的换行符
+    
+    [mutStr replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:range2];
+    
+    return mutStr;
+    
+}
+
+@end

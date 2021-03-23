a12688e1bf4f473777e9427ff0f0281ae3c8731a
sooxie <xiezhiping13179@gmail.com>
2021-03-23 16:00:18 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-XIExtension_Tests/Pods-XIExtension_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XIExtension_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XIExtension_Tests
+@end
---
+++ Example/Pods/Target Support Files/XIExtension/XIExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XIExtension : NSObject
+@end
+@implementation PodsDummy_XIExtension
+@end
